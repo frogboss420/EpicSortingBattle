ca2cfb920c3708e0d666562ffed1e8157fcb85cf
ShadyXGuy <156676283+ShadyXGuy@users.noreply.github.com>
2025-08-20 09:08:29 +0200

    Starter bubbleSort, opstilt while lykke til at se om liste (items aka vores data)

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -21,6 +21,50 @@ import random, tests
 #    print('Sorteret efter {} forsøg'.format(attempts))
 #    return items
 
+#bubbleSort sortringsalgoritme (intermediate)
+def bubbleSort(item):
+    #Kopier den liste, vi modtager som parameter, for ikke at ændrer originale
+    items = items.copy()
+
+    #Boolean til markering af, om listen er sorteret
+    isSorted = None
+
+    #Tællevariabel til at holde styr på antal af forsøg
+    attempts = 0
+
+    #while lykke som kører endtil vores data (item) er sorteret.
+    while not isSorted:
+
+        #tæller antal forsøg
+        attempts += 1
+
+        #'if' sættning som tjek for at stoppe tendensen mod uendeligt
+        if attempts > len(items) * 5000:
+            print('Giver op på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
+            print('Giver op på grund af for mange forsøg ({}) og bruger TimSort'.format(attempts))
+                items.sort()
+                return items
+
+        # 'for' lykke som køre i antallet af enheder
+        # i vores data (items) burde også sørge for vi ikke
+        # ender med at dobbelt tjekke allerede sorteret data
+        for i in range(1, len(items)-i):
+
+            # 'if' sætning til samligning af data (items index i)
+            # ser om indexet er mindre en det forige, samt gør den at vi
+            # ikke har brug for et tjek i slutningen af et loop med at se
+            # på tal vi allerede har tjekket
+            # dette spare lidt resourcer
+            if items[i] < items[i-1]:
+                items[i], items[i-1] = items[i-1], items[i]
+            print(items)
+
+
+
+
+
+
+#quickSort sortringsalgoritme (advanced)
 
 
 if __name__ == '__main__':
